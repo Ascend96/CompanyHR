d4d102d584c98c32c796a38cb2c72944be2b5c72
Ascend96 <mmartens28@yahoo.com>
2020-11-01 13:35:29 -0600

    NLog - dotnet add package NLog.Web.AspNetCore

--- CompanyHR/Program.cs
+++ CompanyHR/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-
+using NLog.Web;
+using System.IO;
 namespace CompanyHR
 {
     class Program
     {
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            logger.Info("Program has Started");
+
             Console.WriteLine("Hello World!");
+
+            logger.Info("Program has ended");
         }
     }
 }
